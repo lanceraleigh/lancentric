43bd3d0621d233f31f07b1b4c307337c24d1c065
lanceshunnii <lanceshunnii@gmail.com>
2022-09-21 23:41:58 -0600

    language fix

---
+++ src/components/login/Auth0PopUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="auth0">
+    <div class="title">Create an Account</div>
+    <div>to save your language learning progress</div>
+    <div class="login-button" @click="login">Signup Free</div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth0.loginWithRedirect();
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.auth0 {
+  z-index: 10;
+}
+</style>
---
+++ src/components/login/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Auth0PopUp></Auth0PopUp>
+  </div>
+</template>
+<script>
+import Auth0PopUp from "../login/Auth0PopUp.vue";
+export default {
+  name: "LoginPage",
+  components: {
+    Auth0PopUp,
+  },
+};
+</script>
+<style lang="scss"></style>
