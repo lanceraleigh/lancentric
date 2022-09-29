94dc5932f1ef14190af0cddd6e682c7a74f9b423
lanceshunnii <lanceshunnii@gmail.com>
2022-09-28 19:59:12 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { createApp } from "vue";
+import { createAuth0 } from "@auth0/auth0-vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
 const app = createApp(App);
-app.use(store).use(router).mount("#app");
+app
+  .use(
+    createAuth0({
+      domain: "domain",
+      client_id: "id",
+      redirect_uri: "redirect",
+    })
+  )
+  .use(store)
+  .use(router)
+  .mount("#app");
