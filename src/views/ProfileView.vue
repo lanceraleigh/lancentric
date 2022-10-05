79a3088c9e07f59600bad3dceb2ee56fa730c004
lanceshunnii <lanceshunnii@gmail.com>
2022-10-04 22:19:07 -0600

    added some progress saving based on the userid and the lesson id also fixed some of the styling and first implementation of the supabase database pt 3

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -12,9 +12,9 @@
       </div>
       <div class="info-item-title">
         Member since:
-        <span class="info-item-value">{{
+        <!-- <span class="info-item-value">{{
           user.created_at.toDateString() || "unknown"
-        }}</span>
+        }}</span> -->
       </div>
       <button @click="logout">Sign Out</button>
     </div>
